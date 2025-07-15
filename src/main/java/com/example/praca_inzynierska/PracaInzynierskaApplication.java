2810f1ec286e410a82c997c5901b4e65f50d40f1
Semwerynek <kaw.seweryn@gmail.com>
2025-07-15 17:54:17 +0200

    Initial commit

---
+++ src/main/java/com/example/praca_inzynierska/PracaInzynierskaApplication.java
@@ -0,0 +1,13 @@
+package com.example.praca_inzynierska;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracaInzynierskaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracaInzynierskaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/praca_inzynierska/PracaInzynierskaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praca_inzynierska;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracaInzynierskaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
