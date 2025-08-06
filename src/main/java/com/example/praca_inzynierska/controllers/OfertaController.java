d80d9eb2fd0d87293ff54b83ac92c928d5ab6780
Semwerynek <kaw.seweryn@gmail.com>
2025-08-06 20:39:11 +0200

    feat: "sklep menu" page with 4 products

---
+++ src/main/java/com/example/praca_inzynierska/controllers/OfertaController.java
@@ -0,0 +1,18 @@
+package com.example.praca_inzynierska.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+* Controller handling "/oferta/**" endpoints
+* */
+@Controller()
+@RequestMapping(value = "/oferta")
+public class OfertaController {
+
+    @GetMapping(value = "/sklep-menu")
+    public String sklep_menu(){
+        return "franchisee/sklep-menu";
+    }
+}
